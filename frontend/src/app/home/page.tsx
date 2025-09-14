cc900f1c0349e5aea736d6f65b4f2a812122b4dd
baolong110904 <baolong110904@gmail.com>
2025-09-15 00:00:40 +0700

    update header and banner

--- frontend/src/app/home/page.tsx
+++ frontend/src/app/home/page.tsx
@@ -1,9 +1,7 @@
 
 "use client";
-import Header from "../../components/Header";
 import Banner from "../../components/Home/Banner";
 import Procedure from "../../components/Home/Procedure";
-import Footer from "../../components/Footer";
 import { useState } from "react";
 import { LangContext } from "../../components/LangContext";
 
@@ -11,13 +9,11 @@ export default function Home() {
   const [lang, setLang] = useState("vi");
   return (
     <LangContext.Provider value={{ lang, setLang }}>
-      <div className="font-sans min-h-screen bg-[#f8fafc] flex flex-col items-center justify-center p-0">
-        <Header />
+      <div className="font-sans min-h-screen bg-[#f8fafc] flex flex-col items-center p-0">
         <Banner />
         <main className="flex flex-col items-center justify-center w-full max-w-3xl py-10 px-4">
           <Procedure />
         </main>
-        <Footer />
       </div>
     </LangContext.Provider>
   );
