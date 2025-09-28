f192f36fcf6ad266df3df940e63f8efb9ef7db15
Minh Quan <quanhugo2505@gmail.com>
2025-09-28 23:40:21 +0700

    deploying cache

--- frontend/next.config.ts
+++ frontend/next.config.ts
@@ -1,7 +1,19 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  async headers() {
+    return [
+      {
+        source: "/:path*\\.(svg|jpg|jpeg|png|gif|webp|avif)",
+        headers: [
+          {
+            key: "Cache-Control",
+            value: "public, max-age=31536000, immutable",
+          },
+        ],
+      },
+    ];
+  },
 };
 
 export default nextConfig;
